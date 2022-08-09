2e511f0010744540f9f1c88c91f9963d0e67e37e
Franco G <francogiannotti115@gmail.com>
2022-08-09 09:05:56 -0300

    Primer version contrato Forestoken ERC20 (#1)
    * add contract files
    
    * change contract code and copy locaqlly open zeppelin files
    
    * add compile2.js
    
    * Code to compile + deploy contract
    
    Co-authored-by: franco.giannotti <franco.giannotti@cabify.com>

---
+++ src/server/contracts/compile.js
@@ -0,0 +1,74 @@
+const path = require("path");
+const solc = require("solc"); //don"t forget to install the right solc version !
+const fs = require("fs-extra");
+
+const CONTRACT_NAME = "Forestoken";
+
+const pathERC20 = "node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+const pathIERC20 = "node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
+const pathIERC20Metadata = "node_modules/@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+const pathContext = "node_modules/@openzeppelin/contracts/utils/Context.sol";
+
+async function main() {
+    const buildPath = path.resolve(__dirname, "build");
+    fs.removeSync(buildPath);
+    fs.ensureDirSync(buildPath);
+    // Compile the source code and retrieve the ABI and Bytecode
+    // These are the only two things we need to deploy a contract
+    const { abi, bytecode } = compileContract(["./","", CONTRACT_NAME+".sol"]);
+
+    // Store the ABI and Bytecode into a JSON file
+    let outputPath = path.resolve(buildPath, CONTRACT_NAME+'.json');
+    fs.outputJsonSync(outputPath, { abi, bytecode }, { spaces: 2 });
+    console.log('----------------- Compiled contract written to ' + CONTRACT_NAME+'.json ----------------- \n');
+}
+
+function compileContract(Contract) {
+    const contractPath = path.resolve(__dirname, ...Contract);
+    const contractSourceCode = fs.readFileSync(contractPath, "utf8");
+
+    var input = {
+        language: "Solidity",
+        sources: {
+            ForestokenContract: {
+                content: contractSourceCode
+            }
+        },
+        settings: {
+            optimizer: {
+                enabled: true
+            },
+            outputSelection: {
+                "*": {
+                    "*": [ "*" ]
+                }
+            }
+        }
+    };
+
+
+    let output = JSON.parse(solc.compile(JSON.stringify(input), { import: findImports }));
+    console.log(output);
+    let contractObject = output.contracts.ForestokenContract[CONTRACT_NAME];
+    return {
+        abi: contractObject.abi,
+        bytecode: contractObject.evm.bytecode.object,
+    };
+}
+
+// Load the contract source code
+const ERC20SourceCode = fs.readFileSync(pathERC20);
+const IERC20SourceCode = fs.readFileSync(pathIERC20);
+const IERC20SourceCodeMetadata = fs.readFileSync(pathIERC20Metadata);
+const ContextSourceCode = fs.readFileSync(pathContext);
+
+function findImports(path) {
+    if (path === pathERC20) return { contents: `${ERC20SourceCode}` };
+    if (path === pathIERC20) return { contents: `${IERC20SourceCode}` };
+    if (path === pathIERC20Metadata) return { contents: `${IERC20SourceCodeMetadata}` };
+    if (path === pathContext) return { contents: `${ContextSourceCode}` };
+    else return { error: "File not found" };
+}
+
+main().then(() => process.exit(0));
+
---
+++ src/server/contracts/deploy.js
@@ -0,0 +1,46 @@
+const Web3 = require("web3");
+
+// Loading the contract ABI and Bytecode
+// (the results of a previous compilation step)
+const fs = require("fs");
+const path = require("path");
+
+async function main() {
+    const buildPath = path.resolve(__dirname, "build");
+    const { abi, bytecode } = JSON.parse(fs.readFileSync(buildPath+"/Forestoken.json"));
+    // Configuring the connection to an Ethereum node
+    const network = process.env.ETHEREUM_NETWORK;
+    const web3 = new Web3(
+        new Web3.providers.HttpProvider(
+            `https://${network}.infura.io/v3/${process.env.INFURA_PROJECT_ID}`
+        )
+    );
+    // Creating a signing account from a private key
+    const signer = web3.eth.accounts.privateKeyToAccount(
+        process.env.SIGNER_PRIVATE_KEY
+    );
+    web3.eth.accounts.wallet.add(signer);
+
+    // Using the signing account to deploy the contract
+    const contract = new web3.eth.Contract(abi);
+    contract.options.data = "0x"+bytecode;
+    let options = { data: "0x"+bytecode, arguments: [signer.address, 1000] };
+    const deployTx = contract.deploy(options);
+    const deployedContract = await deployTx
+        .send({
+            from: signer.address,
+            gas: await deployTx.estimateGas(),
+        })
+        .once("transactionHash", (txhash) => {
+            console.log(`Mining deployment transaction ...`);
+            console.log(`https://${network}.etherscan.io/tx/${txhash}`);
+        });
+    // The contract is now deployed on chain!
+    console.log(`Contract deployed at ${deployedContract.options.address}`);
+    console.log(
+        `Add DEMO_CONTRACT to the.env file to store the contract address: ${deployedContract.options.address}`
+    );
+}
+
+require("dotenv").config();
+main();
