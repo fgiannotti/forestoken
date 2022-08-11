67e5fa212556646a475de5caf4ba9efba76251c9
juanicarramal <31519467+juanicarramal@users.noreply.github.com>
2022-08-10 21:05:01 -0300

    transfer

--- src/server/contracts/call.js
+++ src/server/contracts/call.js
@@ -16,28 +16,61 @@ async function main() {
       `https://${network}.infura.io/v3/${process.env.INFURA_PROJECT_ID}`
     )
   );
+  console.log("Web3 connected", web3.currentProvider);
   console.log(`Connected to ${network}`);
   // Creating a signing account from a private key
   const signer = web3.eth.accounts.privateKeyToAccount(
     process.env.SIGNER_PRIVATE_KEY
   );
   web3.eth.accounts.wallet.add(signer);
+
+  // Creating a signing account from a private key
+  const receiver = web3.eth.accounts.privateKeyToAccount(
+    process.env.RECEIVER_PRIVATE_KEY
+  );
+  web3.eth.accounts.wallet.add(receiver);
+  console.log("Accounts created", web3.eth.accounts.wallet);
+
   // Creating a Contract instance
   const contract = new web3.eth.Contract(
     abi,
     // Replace this with the address of your deployed contract
-    process.env.DEMO_CONTRACT
+    process.env.FORESTOKEN_CONTRACT_ADDRESS
   );
-  // Issuing a transaction that calls the `echo` method
-  console.log(contract.methods);
 
   //Using transaction that obtains the balance of the contract
   const balance = await contract.methods.balanceOf(signer.address).call();
   console.log(`Balance: ${balance}`);
 
   //Using de transaction to show totalSupply of the contract
-  const totalSupply = await contract.methods.totalSupply();
+  const totalSupply = await contract.methods.totalSupply().call();
   console.log(`Total supply: ${totalSupply}`);
+
+  //Using transaction to show name of the contract
+  const name = await contract.methods.name().call()
+  console.log(`Name: ${name}`);
+
+  //Using transaction to show symbol of the contract
+  const symbol = await contract.methods.symbol().call();
+  console.log(`Symbol: ${symbol}`);
+
+  //Using transaction to transfer tokens from one account to another
+  const tx = contract.methods.transfer(
+    receiver.address,
+    1
+  ).send({
+      from: signer.address,
+      gas: 1000000
+    })
+    .once("transactionHash", (txhash) => {
+      console.log(`Mining transaction ...`);
+      console.log(`https://${network}.etherscan.io/tx/${txhash}`);
+    })
+    .on("error", (error) => {
+      console.log(error);
+    });
+  // The transaction is now on chain!
+  //console.log(`Mined in block ${receipt.blockNumber}`);
 
   /*
   const tx = contract.methods.echo("Hello, world!");
