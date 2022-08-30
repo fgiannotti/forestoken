b751c3cd3a9dc9ebb4466abb08af2f025095aa61
franco.giannotti <franco.giannotti@cabify.com>
2022-08-30 15:46:22 -0300

    adding dotenv, and next.config.js changes for webpack to not compile fs or dotenv

--- src/server/modules/app.module.ts
+++ src/server/modules/app.module.ts
@@ -4,13 +4,19 @@ import { RenderModule } from 'nest-next';
 import Next from 'next';
 import { AppController } from '../controllers/app.controller';
 import { AppService } from '../services/app.service';
-import { NODE_ENV } from '../../shared/constants/env';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../entities/user.entity';
 import { UsersService } from '../services/users.service';
 import { UsersModule } from './users.module';
 import { TokensModule } from './tokens.module';
-
+import {
+  DB_HOST,
+  DB_NAME,
+  DB_PASSWORD,
+  DB_PORT,
+  DB_USER,
+  NODE_ENV,
+} from '../../shared/constants/env';
 declare const module: any;
 
 @Module({})
@@ -43,13 +49,20 @@ export class AppModule {
       // TODO: env configuration here for host
       // For docker I use the container name 'forestoken_mysql_1'
       // For local development host is 'localhost'
+      host: DB_HOST,
+      port: Number(DB_PORT),
+      username: DB_USER,
+      password: DB_PASSWORD,
+      database: DB_NAME,
+/*
       host: '127.0.0.1',
       //host: 'forestoken_mysql_1',
       port: 3306,
-      username: 'test',
+      username: 'root',
       // locally i didn't need a password as root
-      password: 'test',
+      password: '',
       database: 'test',
+      */
       entities: [User],
       // to auto create schema, avoid in prod?
       synchronize: true,
