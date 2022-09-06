b0bc51dd6e6caa4f884173e706ac79660caa4594
juanicarramal <juanicarramal@gmail.com>
2022-09-06 20:14:02 -0300

    Me conforme con esto bien basico para avanzar...

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,7 +12,16 @@ import '@fontsource/dm-sans/500.css';
 import '@fontsource/dm-sans/700.css';
 import 'rc-drawer/assets/index.css';
 import 'react-modal-video/css/modal-video.min.css';
+import Head from 'next/head';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import theme from 'src/client/theme/theme';
+import createEmotionCache from 'src/client/theme/createEmotionCache';
+import { useEffect } from 'react';
 
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
 
 class App extends NextApp<AppProps> {
   appData: AppData;
@@ -26,17 +35,42 @@ class App extends NextApp<AppProps> {
   }
 
   render() {
-    const { Component, pageProps } = this.props;
+    const {
+      Component,
+      emotionCache = clientSideEmotionCache,
+      pageProps,
+    } = this.props;
 
     const Layout = Component.layout || (({ children }) => <>{children}</>);
 
+    /*
+    useEffect(() => {
+      // Remove the server-side injected CSS.
+      const jssStyles = document.querySelector('#jss-server-side');
+      if (jssStyles) {
+        jssStyles?.parentElement?.removeChild(jssStyles);
+      }
+    }, []);
+    */
+
     return (
       <AppDataContext.Provider value={this.appData}>
-        <AnimatePresence exitBeforeEnter>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </AnimatePresence>
+        <CacheProvider value={emotionCache}>
+          <Head>
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1, shrink-to-fit=no"
+            />
+          </Head>
+          <ThemeProvider theme={theme}>
+            <AnimatePresence exitBeforeEnter>
+              <Layout>
+                <CssBaseline />
+                <Component {...pageProps} />
+              </Layout>
+            </AnimatePresence>
+          </ThemeProvider>
+        </CacheProvider>
       </AppDataContext.Provider>
     );
   }
