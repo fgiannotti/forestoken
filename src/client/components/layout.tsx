52b92dc8e730caee8f3ee6c3b216f670f00dea15
juanicarramal <juanicarramal@gmail.com>
2022-09-09 04:13:02 -0300

    Sticky de layout landing con useState

--- src/client/components/layout.tsx
+++ src/client/components/layout.tsx
@@ -1,46 +1,27 @@
-/** @jsxRuntime classic */
-/** @jsx jsx */
-import { jsx } from 'theme-ui';
-import React, { useCallback } from 'react';
+import React, { useState } from 'react';
 import Sticky from 'react-stickynode';
-import { useStickyState } from 'src/client/contexts/app/app.provider';
 import { Waypoint } from 'react-waypoint';
-import { useStickyDispatch } from 'src/client/contexts/app/app.provider';
 
 import Header from './header/header';
 import Footer from './footer/footer';
 export default function Layout({ children }) {
-  const isSticky = useStickyState('isSticky');
-  const dispatch = useStickyDispatch();
-  const setSticky = useCallback(
-    () => dispatch({ type: 'SET_STICKY' }),
-    [dispatch],
-  );
-
-  const removeSticky = useCallback(
-    () => dispatch({ type: 'REMOVE_STICKY' }),
-    [dispatch],
-  );
+  const [sticky, setSticky] = useState(false);
 
   const onWaypointPositionChange = ({ currentPosition }) => {
     if (currentPosition === 'above') {
-      setSticky();
+      setSticky(true);
     }
     if (currentPosition === 'below') {
-      removeSticky();
+      setSticky(false);
     }
   };
 
   return (
     <React.Fragment>
-      <Sticky enabled={isSticky} innerZ={991}>
-        <Header className={`${isSticky ? 'sticky' : 'unSticky'}`} />
+      <Sticky enabled={sticky} innerZ={991}>
+        <Header className={`${sticky ? 'sticky' : 'unSticky'}`} />
       </Sticky>
-      <Waypoint
-        onEnter={removeSticky}
-        // onLeave={setSticky}
-        onPositionChange={onWaypointPositionChange}
-      />
+      <Waypoint onPositionChange={onWaypointPositionChange} />
       <main
         sx={{
           variant: 'layout.main',
