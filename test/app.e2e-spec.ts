e10be73a319c281398f6bcfd1e5de4ad1bd680bf
AzulLuquet <mariaazul123@gmail.com>
2022-09-22 17:21:36 -0300

    Cambios en los tests de post usuario

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,6 +1,7 @@
+/// <reference types="jest" />
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import request from 'supertest';
 import { AppModule } from '../src/server/modules/app.module';
 
 describe('AppController (e2e)', () => {
@@ -12,13 +13,30 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    
+    app.useGlobalPipes(new ValidationPipe({
+      disableErrorMessages: true,
+      transform: true,
+    }));
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('POST /users', () => {
     return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .post('127.0.0.1:3000/users')
+      .send({
+        name:"prueba 15",
+        mail:"prueba1@gmail.com",
+        dni:"12345678",
+        provincia:"CABA",
+        ciudad:"CABA",
+        direccion:"Juan B Alberdi",
+        codigoPostal:"14245", //debería ser 4 numeros
+        responsableTributo:"Monotributista",
+        personaPolitica:false,
+        personaRegulada:true,
+        fechaNacimiento:"1999-03-13",
+        urlFoto:"flaticon.com/free-icon/wood_1059509?term=wood&page=1&position=5&page=1&position=5&related_id=1059509&origin=search"
+      }).expect(400);
   });
 });
--- test/test-utils.ts
+++ test/test-utils.ts
@@ -2,6 +2,7 @@ import { UserDto } from '../src/server/dtos/user.dto';
 import { User } from '../src/server/entities/user.entity';
 import { MovementDto } from '../src/server/dtos/movement.dto';
 import { Movement } from '../src/server/entities/movement.entity';
+import { Wallet } from 'src/server/entities/wallet.entity';
 
 export function createMockUserDto(): UserDto {
   return {
@@ -61,4 +62,13 @@ export function createMockMovement(): Movement {
     burned: false,
     amount: 101,
   } as Movement;
+}
+
+export function createMockWallet(): Wallet {
+  const user = createMockUser();
+  return {
+    address: '0xDF3e2d7650A6C17F102D51Db58B280580F825371',
+    privateKey: 'afdfd9c3d2095ef696594f6cedcae59e72dcd697e2a7521b1578140422a4f890',
+    userId: user.id,
+  } as Wallet;
 }
