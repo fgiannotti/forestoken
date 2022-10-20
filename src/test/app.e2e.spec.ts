6fa7d99b5bed8523370a9a3e1a6362d3a27a6fe1
AzulLuquet <mariaazul123@gmail.com>
2022-10-19 23:26:07 -0300

    arreglo errores que no dejaban correr los test

--- src/test/app.e2e.spec.ts
+++ src/test/app.e2e.spec.ts
@@ -30,6 +30,7 @@ import {
   createInvalidBooleans,
   createInvalidDates,
   createInvalidUrls,
+  createMockMovement,
 } from './test-utils';
 import { PoWR } from "../server/entities/powr.entity";
 
@@ -296,18 +297,18 @@ async function createTestModuleWithMockDB() {
     .useFactory({
       factory: () => ({
         create: jest.fn(
-          () => new Promise((resolve) => resolve(createMockUser())),
+          () => new Promise((resolve) => resolve(createMockMovement())),
         ),
         find: jest.fn(
-          () => new Promise((resolve) => resolve([createMockUser()])),
+          () => new Promise((resolve) => resolve([createMockMovement()])),
         ),
         update: jest.fn(
-          (id, project2) => new Promise((resolve) => resolve(createMockUser())),
+          (id, project2) => new Promise((resolve) => resolve(createMockMovement())),
         ),
         findOne: jest.fn(
           ({ uuid }) =>
             new Promise((resolve) => {
-              resolve(createMockUser());
+              resolve(createMockMovement());
             }),
         ),
         delete: jest.fn(),
