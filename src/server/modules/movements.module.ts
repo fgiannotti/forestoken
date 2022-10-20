13d4288af9bc083e3cf54b2c2de5144e6d3bc0e1
AzulLuquet <mariaazul123@gmail.com>
2022-10-20 12:29:11 -0300

    cambios por review de PR (saco cosas innecesarias)

--- src/server/modules/movements.module.ts
+++ src/server/modules/movements.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
-import { User } from '../entities/user.entity';
 import { Movement } from '../entities/movement.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Wallet } from '../entities/wallet.entity';
 import { MovementsController } from '../controllers/movements.controller';
 import { MovementsService } from '../services/movements.service';
 
 @Module({
   controllers: [MovementsController],
-  imports: [TypeOrmModule.forFeature([User, Movement, Wallet])],
+  imports: [TypeOrmModule.forFeature([Movement])],
   providers: [MovementsService],
   exports: [MovementsService],
 })
