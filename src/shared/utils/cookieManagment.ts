a00b5f5b987b17f17db593a25f5d596cf6f2cd63
AzulLuquet <mariaazul123@gmail.com>
2022-10-27 13:54:00 -0300

    Genero una función para el manejo de cookies

---
+++ src/shared/utils/cookieManagment.ts
@@ -0,0 +1,5 @@
+export const createCookie = (name: string, options?: {}) => {
+    
+}, deleteCookie = (name: string, context = document) => {
+    context.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
