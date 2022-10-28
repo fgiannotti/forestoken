33fc5e9b5bd0fcb2a3197a68eb15c1f9529af7a4
AzulLuquet <mariaazul123@gmail.com>
2022-10-28 16:43:40 -0300

    creo service para hasear archivos con sha256
    Le agrego el 0x adelante para que se sepa que es hexadecimal (así lo necesita el smartcontract)

--- src/server/services/file.service.ts
+++ src/server/services/file.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { createReadStream } from 'fs';
+import * as crypto from 'crypto';
+import * as fs from 'fs';
 
 @Injectable()
 export class FileService {
   async getFile(path: string): Promise<any> {
     return createReadStream(`${path}`);
+  }
+
+  async hashFile(path: string): Promise<any> {
+    const file = fs.readFileSync(path);
+    
+    return '0x' + await crypto
+      .createHash('sha256')
+      .update(file)
+      .digest('hex');
   }
 }
