51f37d42e3a45eda2cec4bdf3bee1e7e3a82ecff
juanicarramal <juanicarramal@gmail.com>
2022-10-31 10:20:52 -0300

    build ok

--- src/pages/accreditation/components/StepForm/ComercialContract.tsx
+++ src/pages/accreditation/components/StepForm/ComercialContract.tsx
@@ -1,22 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import PDFActions from '../PDF/PDFActions';
 import ComercialContractPDF from '../PDF/ComercialContractPDF';
 
 export default function ComercialContract({ handleNext, handleBack, values }) {
+  const [isRendered, setIsRendered] = React.useState(false);
   const handleSubmit = () => {
     handleNext();
   };
 
+  useEffect(() => {
+    setIsRendered(true);
+  }, []);
+
   return (
     <form onSubmit={handleSubmit}>
       <Box my={3}>
-        <PDFActions
-          fileName={'Contrato comercial Forestoken.pdf'}
-          PDFdoc={<ComercialContractPDF values={values} />}
-          initialValue={true}
-        />
+        {isRendered && (
+          <PDFActions
+            fileName={'Contrato comercial Forestoken.pdf'}
+            PDFdoc={<ComercialContractPDF values={values} />}
+            initialValue={true}
+          />
+        )}
 
         <Box sx={{ display: 'flex', justifyContent: 'flex-end', mt: 3 }}>
           <Button sx={{ mr: 1 }} onClick={() => handleBack()}>
