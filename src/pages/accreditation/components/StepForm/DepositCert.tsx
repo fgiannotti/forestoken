c0c878d82cf63a4bf78bfc55197a6fd64c62e10e
juanicarramal <juanicarramal@gmail.com>
2022-11-02 20:20:44 -0300

    fix ComercialContractPDF

--- src/pages/accreditation/components/StepForm/DepositCert.tsx
+++ src/pages/accreditation/components/StepForm/DepositCert.tsx
@@ -58,7 +58,10 @@ export default function DepositCert({
             type="file"
             inputProps={{ accept: 'application/pdf' }}
             onChange={({ target }) =>
-              setValues({ ...values, pdf: target?.files[0] })
+              setValues({
+                ...values,
+                pdf: (target as HTMLInputElement)?.files[0],
+              })
             }
             required
           />
