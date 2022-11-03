fecb1e4b4bf03de14d0e95d7e2d18b7cf5006e92
SantiagoAspres <santi.aspres@gmail.com>
2022-11-02 21:27:29 -0300

    bloqueo mas rutas de los controllers

--- src/server/modules/auth.module.ts
+++ src/server/modules/auth.module.ts
@@ -1,20 +1,19 @@
-import {
-  HttpModule,
-  MiddlewareConsumer,
-  Module,
-  NestModule,
-  RequestMethod,
-} from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from '../entities/user.entity';
-import { AuthController } from '../controllers/auth/auth.controller';
-import { AuthService } from '../services/auth.service';
-import { GoogleStrategy } from '../strategies/google.strategy';
-import { SessionSerializer } from '../../shared/utils/Serializer';
-import { LoggerMiddleware } from '../middleware/verifyUser.middleware';
-import { AppController } from '../controllers/app.controller';
-import { TokensController } from '../controllers/tokens.controller';
-import { UsersService } from '../services/users.service';
+import {HttpModule, MiddlewareConsumer, Module, NestModule, RequestMethod,} from '@nestjs/common';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {User} from '../entities/user.entity';
+import {AuthController} from '../controllers/auth/auth.controller';
+import {AuthService} from '../services/auth.service';
+import {GoogleStrategy} from '../strategies/google.strategy';
+import {SessionSerializer} from '../../shared/utils/Serializer';
+import {LoggerMiddleware} from '../middleware/verifyUser.middleware';
+import {AppController} from '../controllers/app.controller';
+import {TokensController} from '../controllers/tokens.controller';
+import {UsersService} from '../services/users.service';
+import {PaymentsController} from "../controllers/payments.controller";
+import {MovementsController} from "../controllers/movements.controller";
+import {FilesController} from "../controllers/files.controller";
+import {AffiliatesController} from "../controllers/affiliates.controller";
+import {UsersController} from "../controllers/users.controller";
 
 @Module({
   imports: [TypeOrmModule.forFeature([User]), HttpModule],
@@ -37,7 +36,8 @@ export class AuthModule implements NestModule {
       .exclude(
         { path: '/', method: RequestMethod.GET },
         { path: '/auth/(.*)', method: RequestMethod.GET },
+          {path:'/users',method: RequestMethod.POST}
       )
-      .forRoutes(AppController, TokensController);
+      .forRoutes(AppController, TokensController, PaymentsController, MovementsController, FilesController, AffiliatesController, UsersController);
   }
 }
