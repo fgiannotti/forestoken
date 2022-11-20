6cddd3d4525fc2bdfc0cbf2fa9e08265f492fc61
AzulLuquet <mariaazul123@gmail.com>
2022-11-20 19:02:34 -0300

    Ajusto filtro de página admin
    Agrego la lógica para que la tabla de admin muestre las solicitudes y se pueda filtrar por estado.

--- src/pages/admin/accreditation-requests.tsx
+++ src/pages/admin/accreditation-requests.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import Link from 'next/link';
 import ZoomInIcon from '@mui/icons-material/ZoomIn';
-import { Button } from '@mui/material';
-import { GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
-import MovementsList from '../../client/components/MovementsList';
+import { Box, Button, Typography } from '@mui/material';
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import SelectEstadoSolicitud from '../../client/components/SelectAccreditationState';
+import axios from 'axios';
 
 const columns: GridColDef[] = [
   {
@@ -59,13 +60,47 @@ const columns: GridColDef[] = [
 ];
 
 const AccreditationRequests = ({ accreditations = [] }) => {
+  const [stateFilter, setStateFilter] = React.useState('all'),
+    [stateFiltered, setStateFiltered] = React.useState(accreditations);
+
+  console.log(stateFilter != 'all'? `/accreditations/admin/${stateFilter}`: `/accreditations/admin/all`)
+
+  React.useEffect(() => {
+    axios
+      .get(
+        `${stateFilter != 'all'
+            ? `/accreditations/admin/${stateFilter}`
+            : `/accreditations/admin/all`
+        }`,
+      )
+      .then((res) => {
+        setStateFiltered(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [stateFilter]);
+  
   return (
     <div>
-      <MovementsList
-        movements={accreditations}
-        columns={columns}
-        title={'Solicitudes de acreditaciones pendientes de aprobación'}
-      />
+      <div style={styles.header}>
+        <Typography component="h2" variant="h6" sx={styles.title} gutterBottom>
+          Solicitudes de acreditaciones pendientes de aprobación
+        </Typography>
+        <SelectEstadoSolicitud
+          stateFilter={stateFilter}
+          setStateFilter={setStateFilter}
+        />
+      </div>
+      <Box sx={{ height: 500, backgroundColor: 'white' }}>
+        <DataGrid
+          rows={stateFiltered}
+          columns={columns}
+          pageSize={7}
+          rowsPerPageOptions={[7]}
+          disableSelectionOnClick
+        />
+      </Box>
     </div>
   );
 };
@@ -77,5 +112,16 @@ const styles = {
     display: 'flex',
     justifyContent: 'end',
     alignItems: 'center',
+  },
+  header: {
+    display: 'flex',
+    flexDirection: 'row' as const,
+    justifyContent: 'space-between',
+    paddingTop: '5%',
+  },
+  title: {
+    fontWeight: '400',
+    fontSize: '1.5rem',
+    color: 'gray',
   },
 };
