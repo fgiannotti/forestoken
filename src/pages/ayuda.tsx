7d3f71ae186cec6e8f2ca237b6778810fd7e3a9f
juanicarramal <juanicarramal@gmail.com>
2022-11-24 11:11:51 -0300

    fix userDataContext

--- src/pages/ayuda.tsx
+++ src/pages/ayuda.tsx
@@ -1,14 +1,39 @@
-import { Typography } from '@mui/material';
 import ControlledAccordions from 'src/client/components/ayuda/accordion';
 import Layout from '../client/layouts/dashboard';
 import FAQ from '../client/components/ayuda/ayuda.data';
+import { buildServerSideProps } from '../client/ssr/buildServerSideProps';
+import { UserDataContext } from 'src/client/ssr/userData';
 
-const Ayuda = () => {
+const Ayuda = ({ userData }) => {
   return (
-    <Layout>
-      <ControlledAccordions title={'Centro de ayuda'} data={FAQ}></ControlledAccordions>
-    </Layout>
+    <UserDataContext.Provider value={{ user: userData }}>
+      <Layout>
+        <ControlledAccordions
+          title={'Centro de ayuda'}
+          data={FAQ}
+        ></ControlledAccordions>
+      </Layout>
+    </UserDataContext.Provider>
   );
 };
+
+export const getServerSideProps = buildServerSideProps<any, any>(
+  async (ctx) => {
+    const { userData } = ctx.req.cookies;
+    const [, userId, , userImage, , userName] = userData
+      ? userData.split('|')
+      : [];
+    if (!userId) {
+      console.log('no se recibio la cookie');
+    }
+    return {
+      userData: {
+        user: userId,
+        name: userName,
+        image: userImage,
+      },
+    };
+  },
+);
 
 export default Ayuda;
